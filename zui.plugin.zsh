53ebd2ec71f5a38f75ed84c051cb3cd1988e5727
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2017-03-09 10:33:13 +0100

    zui-demo-fly

--- zui.plugin.zsh
+++ zui.plugin.zsh
@@ -27,7 +27,7 @@ fi
 
 autoload -- zui-list zui-list-draw zui-list-input zui-list-wrapper -zui-log zui-event-loop
 autoload -- zui-process-buffer zui-process-buffer2 zui-usetty-wrapper
-autoload -- zui-demo zui-demo-hello-world zui-demo-text-fields
+autoload -- zui-demo zui-demo-hello-world zui-demo-text-fields zui-demo-fly
 
 zle -N zui-demo
 bindkey "^O^Z" zui-demo
